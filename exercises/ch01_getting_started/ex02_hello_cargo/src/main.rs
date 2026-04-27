f7da294b15741fca24ab7d8e139c15d6303fc2f7
housniabdellatif <abdellatife.housni@gmail.com>
2026-04-27 12:48:38 +0100

    solved exercice

--- exercises/ch01_getting_started/ex02_hello_cargo/src/main.rs
+++ exercises/ch01_getting_started/ex02_hello_cargo/src/main.rs
@@ -3,10 +3,10 @@
 
 fn main() {
     // TODO: Print "Hello, Cargo!" in green color
+    println!("{}", "Hello, Cargo!".green());
     // TODO: Print "This is a Rust project managed by Cargo!" in blue color
-    //
+    println!("{}", "This is a Rust project managed by Cargo!".blue());
     // Example syntax: println!("{}", "text".green());
-    //
     // Remember to:
     // 1. Add the colored dependency to Cargo.toml
     // 2. Import the colored crate with: use colored::*;
